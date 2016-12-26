06c9796cd3f76516ed91f7533d4735a1f665add7
vagnercazarotto <vagnercazarotto@gmail.com>
2016-12-26 14:44:27 -0200

    Efficient approximation without losing accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print len(features_train[0]) 
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+print clf.score(features_test,labels_test)
+
 
 #########################################################
 
