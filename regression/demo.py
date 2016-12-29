c3d03b1c120d99fd1992ef788124813e8925c470
vagnercazarotto <vagnercazarotto@gmail.com>
2016-12-29 18:02:21 -0200

    Finance Regression
    Using linear regression to see the relation between salary, bonus and
    long term incentive

--- regression/demo.py
+++ regression/demo.py
@@ -5,21 +5,17 @@ regClf.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
 print regClf.coef_
 
 
-
-reg = linear_model.Ridge (alpha = .5)
-reg.fit ([[0, 0], [0, 0], [1, 1]], [0, .1, 1]) 
+reg = linear_model.Ridge(alpha=.5)
+reg.fit([[0, 0], [0, 0], [1, 1]], [0, .1, 1])
 
 
 reg.coef_
 
-print reg.intercept_ 
-
-
+print reg.intercept_
 
 
 reg = linear_model.LassoLars(alpha=.1)
-reg.fit([[0, 0], [1, 1]], [0, 1])  
+reg.fit([[0, 0], [1, 1]], [0, 1])
 
 
-
-print reg.coef_   
+print reg.coef_
