9b8cabf0a6d0531e727d100e890c3e7e58f8d866
vagnercazarotto <vagnercazarotto@gmail.com>
2017-03-25 18:18:06 -0300

    Train test split in R

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -25,11 +25,12 @@ dataset$Purchased = factor(dataset$Purchased,
 
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-"""
+## Only install one time ant them active in the packages panel
+#install.packages('caTools')
 library(caTools)
+
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
