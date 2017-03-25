4eb5bb435f49ab699813dd9747968912cbc3e94f
vagnercazarotto <vagnercazarotto@gmail.com>
2017-03-25 20:14:38 -0300

    Cleaning the templates

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -11,40 +11,21 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 
-#Taking care of missing Data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values= 'NaN',strategy='mean',axis = 0)
-imputer.fit(X[:,1:3])
-X[:,1:3] = imputer.transform(X[:,1:3])
-
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder , OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-## create a hot enconder to separate the labels from variables
-onehotencoder = OneHotEncoder(categorical_features= [0])
-X = onehotencoder.fit_transform(X).toarray()
-
-## let's take care of the second variable which is purchase one
-labelencoder_Y = LabelEncoder()
-y = labelencoder_Y.fit_transform(y)
-
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-
+"""
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-
 ## Fit and transform 
 X_train = sc_X.fit_transform(X_train)
 ## only transform because it's already fit
 X_test = sc_X.transform(X_test)
+"""
 
 
-##Obs: it's a good practice transform the dummy variables
+
+
 
