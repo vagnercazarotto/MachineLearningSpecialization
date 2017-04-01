e14a991dc184a472af105d9eb9d77eb4186653f0
vagnercazarotto <vagnercazarotto@gmail.com>
2017-04-01 14:30:54 -0300

    Fitting the model with R and gettind info from classifier

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/linearRegression.R
@@ -0,0 +1,22 @@
+# Data Preprocessing Template -- Template
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+
+## geting linear model info
+summary(regressor)
