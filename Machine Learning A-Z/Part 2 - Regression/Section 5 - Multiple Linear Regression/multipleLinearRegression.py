64f5cef7113f5198f28ec2f6c5950ca130f5d53e
vagnercazarotto <vagnercazarotto@gmail.com>
2017-04-01 21:37:22 -0300

    Implemented multiple linear regression model

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multipleLinearRegression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multipleLinearRegression.py
@@ -26,6 +26,10 @@ onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 
+## Avoiding the Dummy Variable Trap..
+X = X[:, 1:]
+
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -37,3 +41,8 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+## Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
