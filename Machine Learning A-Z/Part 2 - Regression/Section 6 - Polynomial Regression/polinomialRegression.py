5e23b143fc8d60aa2f1f738b3c786abf96251fe4
vagnercazarotto <vagnercazarotto@gmail.com>
2017-04-03 00:20:10 -0300

    In this case we will use the entire dataset for training and don't use feature scaling because the model will consider as a polynomial

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/polinomialRegression.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 23:39:13 2017
+
+@author: vagnercazarotto
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+## the independent tariable sould be a matriz [:, 1:2]
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+#from sklearn.cross_validation import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
