ce97a96e115772cd8ff25d085ddbd41ce830d2a0
vagnercazarotto <vagnercazarotto@gmail.com>
2017-04-05 12:19:09 -0300

    Decision Tree for Python, this is not a good movel for few features (it don't fit well)

--- Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decisionTree.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/decisionTree.py
@@ -30,7 +30,10 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
@@ -38,7 +41,7 @@ y_pred = regressor.predict(6.5)
 # Visualising the Decision Tree Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Decision Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
@@ -48,7 +51,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Decision Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
