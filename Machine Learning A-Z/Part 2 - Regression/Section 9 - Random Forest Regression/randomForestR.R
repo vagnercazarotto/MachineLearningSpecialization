671b36d0f13c974bd7f90b5db7b26f6d8f7ad0b4
vagnercazarotto <vagnercazarotto@gmail.com>
2017-04-25 12:02:18 -0300

    Random Forest Regression for R

--- Machine Learning A-Z/Part 2 - Regression/Section 9 - Random Forest Regression/randomForestR.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 9 - Random Forest Regression/randomForestR.R
@@ -16,13 +16,21 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
+# Fitting the Random Forest Model to the dataset
 # Create your regressor here
+
+# install.packages('randomForest')
+library(randomForest)
+set.seed(seed = 1234)
+regressor = randomForest(x = dataset[1],## select the independent variable
+                         y = dataset$Salary, ## Depentent variable  
+                         ntree = 100)
+
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
+# Visualising the Random Forest Model results
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
@@ -30,19 +38,19 @@ ggplot() +
              colour = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Random Forest Model)') +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
+# Visualising the Random Forest Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Random Forest Model)') +
   xlab('Level') +
   ylab('Salary')
